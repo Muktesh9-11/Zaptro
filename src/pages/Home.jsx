f9e32ac03fe07faf6957c762df76ac1f48c21607
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2025-07-28 18:56:19 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Carousel from '../components/Carousel'
+import Features from '../components/Features'
+import MidBanner from '../components/MidBanner'
+
+const Home = () => {
+  return (
+    <div>
+      <Carousel />
+      <MidBanner />
+      <Features />
+    </div>
+  )
+}
+
+export default Home
